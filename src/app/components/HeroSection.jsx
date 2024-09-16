3e65796a99279881f452d510e2f7f11f322c7d79
Safwan3528 <tokrimauhitam@gmail.com>
2024-09-16 18:56:30 +0800

    update react type animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
@@ -9,22 +11,44 @@ const HeroSection = () => {
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-400 to bg-orange-600">
               Hello I'm{""}
+              <br />
             </span>
-            Safwan Rahimi
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Safwan Rahimi",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                "Software Engineer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl">
-            Nama saya safwan saya suka coding
+            I'm currently looking for new opportunities, my inbox is always
+            open. Whether you have a question or just want to say hi, I'll try
+            my best to get back to you!
           </p>
+          <br />
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 via-red-500 to bg-orange-500 bg-white hover:bg-slate-200 text-white">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-600 via-orange-700 to bg-orange-600 hover:bg-slate-200 text-white">
               Hire Me
             </button>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
-              Download CV
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-600 via-orange-700 to-orange-600 hover:bg-slate-800 text-white border mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px5 py-2">
+                Download CV
+              </span>
             </button>
           </div>
         </div>
         <div className="col-span-5">
+          <br />
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.png"
