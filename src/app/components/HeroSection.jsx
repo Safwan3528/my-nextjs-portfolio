3b4f7126a3760af99fbf4b6ecb3a83fb7192e1ee
Safwan3528 <tokrimauhitam@gmail.com>
2024-09-16 20:50:41 +0800

    NextJS Portfolio

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -31,9 +31,8 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl">
-            I'm currently looking for new opportunities, my inbox is always
-            open. Whether you have a question or just want to say hi, I'll try
-            my best to get back to you!
+            I excel at crafting elegant digital experiance and i am Proficient
+            in various programming languages and Technologies.
           </p>
           <br />
           <div>
